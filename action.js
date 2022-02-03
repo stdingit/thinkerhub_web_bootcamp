b99c04d7a701b855f4fd0879f6abb12a60f2c1b4
stdingit <96324021+stdingit@users.noreply.github.com>
2022-02-03 10:13:29 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,16 @@
+function Changetoblue() {
+    document.body.style.backgroundColor = "blue";
+    document.getElementById("title").innerHTML = "green";
+}
+
+function Changetogreen() {
+    document.body.style.backgroundColor = "green";
+}
+
+function Changetoyellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function Changetored() {
+    document.body.style.backgroundColor = "red";
+}
